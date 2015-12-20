d791b389dece20f8a5a0904e8d55b4c650219f8e
Ian Carroll <ian@ian.sh>
2015-12-19 23:00:09 -0500

    Completely erradicate Raven.

--- config/app.php
+++ config/app.php
@@ -146,7 +146,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Socialite\SocialiteServiceProvider::class,
-        Jenssegers\Raven\RavenServiceProvider::class,
     ],
 
     /*
