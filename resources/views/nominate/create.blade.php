05b18b923becebe26a5c8dc2aeb88e6a83ff4b04
Ian Carroll <ian@ian.sh>
2015-12-20 23:38:38 -0500

    Update the view.

--- resources/views/nominate/create.blade.php
+++ resources/views/nominate/create.blade.php
@@ -1,7 +1,7 @@
 @extends("app")
 
 @section("content")
-<p>You can nominate one user to be put on the ballot per term. Your nomination does not bind you to voting for them, and they must accept the nomination to be placed on the ballot. The user you nominate must have logged in to this application before you can nominate them.</p>
+<p>You can nominate {{ env('NUM_NOMINATIONS', 2) }} users to be put on the ballot per term. Your nomination does not bind you to voting for them, and they must accept the nomination to be placed on the ballot. The user you nominate must have logged in to this application before you can nominate them.</p>
 <form method="post">
     <input type="text" name="facebook_id" placeholder="Facebook ID">
     {!! csrf_field() !!}
