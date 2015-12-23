79ef09024c816f5207a448c5f0dad584a1984ff5
Ian Carroll <iangcarroll+spam-only@icloud.com>
2015-12-23 17:03:51 -0500

    Update the waiting view to show two nominations.

--- resources/views/candidate/waiting.blade.php
+++ resources/views/candidate/waiting.blade.php
@@ -3,6 +3,6 @@
 @section("content")
 <div class="blankslate">
   <h3>You need more nominations.</h3>
-  <p>You cannot mark yourself as running until three people enter your Facebook ID <i>{{ Auth::user()->facebook_id }}</i> in the Nominate tab.</p>
+  <p>You cannot mark yourself as running until two people enter your Facebook ID <i>{{ Auth::user()->facebook_id }}</i> in the Nominate tab.</p>
 </div>
 @endsection
