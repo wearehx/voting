9613e5550ba8d430362cc403a35e65a45a924d40
Ian Carroll <iangcarroll+spam-only@icloud.com>
2015-12-27 21:32:26 -0500

    can_vote migration

---
+++ database/migrations/2015_12_28_010822_add_can_vote_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCanVoteToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('can_vote')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('can_vote');
+        });
+    }
+}
