861ed6ec066914c08ef73a167b2f34b8161a8c69
Ian Carroll <ian@certly.io>
2016-01-03 02:51:14 -0500

    Ensure the homepage doesn't fall apart in 16 hours.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
 @section("content")
 <h2 class="uuid">UUID</h2>
 <p>When you cast your first vote, a pseudo-random v4 UUID is generated and attached to your account. When you vote in an election, your UUID and vote is made available for download. You may independently calculate the winner of the election and validate your vote was correctly counted. It is important that you only disclose your UUID when you intend for others to see your past, current, and future votes. @if (! Auth::user()->uuid) <b>You do not have a UUID attached to your account.</b> @else <b>Your UUID is <code>{{ Auth::user()->uuid }}</code>.</b> @endif</p>
+@if(nextTerm())
 <h2>Next Election</h2>
 <p>The first election will elect <b>{{ env("NUM_ADMINS") }}</b> admins and will accept nominations starting <b title="{{ nextTerm()->starts_at->subDays(14) }}">{{ nextTerm()->starts_at->subDays(14)->diffForHumans() }}</b>. Voting will begin <b title="{{ nextTerm()->starts_at->subDays(7) }}">{{ nextTerm()->starts_at->subDays(7)->diffForHumans() }}</b>, and the term will start <b title="{{ nextTerm()->starts_at }}">{{ nextTerm()->starts_at->diffForHumans() }}</b>.</p>
+@endif
 @endsection
