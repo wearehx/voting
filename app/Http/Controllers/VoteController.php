6af5fa44c0febffdaae01846b4b1a6353f39d511
Ian Carroll <iangcarroll+spam-only@icloud.com>
2016-02-15 17:27:50 -0500

    Whitespace fixes.

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -19,7 +19,7 @@ class VoteController extends Controller
         $candidates = nextTerm()->candidates()->get()->all();
 
         $this->shuffle($candidates);
-        
+
         return view('vote.cast')
             ->withCandidates($candidates)
             ->withCount(0);
@@ -37,15 +37,15 @@ class VoteController extends Controller
         $this->validate($request, [
             'vote' => 'required|array|vote_count|vote_unique|sane_votes',
         ]);
-        
+
         $user = $request->user();
-        
+
         if ($user->uuid === null) {
             $user->update([
                 'uuid' => uuid(),
             ]);
         }
-        
+
         foreach ($request->get('vote') as $vote) {
             Vote::create([
                 'candidate_id' => Candidate::findOrFail($vote)->id,
@@ -58,7 +58,7 @@ class VoteController extends Controller
 
         return redirect('/');
     }
-    
+
     /**
      * Randomize the given array.
      *
@@ -71,13 +71,13 @@ class VoteController extends Controller
         for ($index = count($candidates) - 1; $index > 0; $index--) {
             /* Pick a random number within the untouched bounds of the array. */
             $int = random_int(0, $index);
-            
+
             /* Set $old to the current $index. */
             $old = $candidates[$index];
-            
+
             /* Set $candidates[$index] to a random value from $items that has an index of < $index. */
             $candidates[$index] = $candidates[$int];
-            
+
             /* Set the randomized key's value ($candidates[$int]) to the old value from the iterated index ($index). */
             $candidates[$int] = $old;
         }
