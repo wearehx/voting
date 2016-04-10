534162eaad8650171b6e34d8a0df1ff23bcf5678
Ian Carroll <ian@ian.sh>
2016-04-10 15:31:02 -0400

    Raven

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Jenssegers\Raven\RavenServiceProvider::class,
     ],
 
     /*
